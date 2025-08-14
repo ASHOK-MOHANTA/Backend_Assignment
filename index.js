447ddd84b04e046e041cbd05b8b042f2907f0f70
ASHOK-MOHANTA <subendum25@gmail.com>
2025-08-14 15:12:10 +0530

    2nd Question

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-const isPrime = require("./isPrime");
+// const isPrime = require("./isPrime");
 
-const nums = [2,5,8,10,15,17,19,"ashok"];
+// const nums = [2,5,8,10,15,17,19,"ashok"];
 
-nums.forEach(num =>{
-    if(isPrime(num)){
-        console.log(`${num} is a prime number`);
-    }else{
-        console.log(`${num} is not a prime number`);
-    }
-})
+// nums.forEach(num =>{
+//     if(isPrime(num)){
+//         console.log(`${num} is a prime number`);
+//     }else{
+//         console.log(`${num} is not a prime number`);
+//     }
+// })
+
+const factorial = require("./factorial");
+
+console.log(factorial(5))
